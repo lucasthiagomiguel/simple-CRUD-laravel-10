e52476d00def302a9e27c75ac0d5d20d1749b4f3
lucas pasquali <lucas.souza@pasqualisolution.com.br>
2024-02-12 11:22:51 -0300

    fixing code

--- database/migrations/2024_02_09_011526_create_places_table.php
+++ database/migrations/2024_02_09_011526_create_places_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('slug');
             $table->string('city');
-            $table->string('state')->unique();
+            $table->string('state');
             $table->boolean('status');
             $table->timestamps();
         });
